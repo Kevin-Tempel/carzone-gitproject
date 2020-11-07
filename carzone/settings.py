3f7d20ad871a1785bb7cc41cbc3d431387f53bcb
Kevin Tempel <kevintempel@Kevins-MacBook-Air.local>
2020-11-07 15:20:13 -0800

    Completed Team Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': '1Ashley!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +119,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Team
+from django.utils.html import format_html
+
 
 # Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html(f'<img src="{object.photo.url}" width="40" style="border-radius: 50%;" />')
+
+    thumbnail.short_description = 'Photo'
+
+    list_display        = ('id', 'thumbnail', 'first_name', 'designation', 'created_date')
+    list_display_links  = ('id', 'thumbnail', 'first_name')
+    search_fields       = ('first_name', 'last_name', 'designation')
+    list_filter         = ('designation',)
+    class Meta:
+        model = Team
+        fields = '__all__'
